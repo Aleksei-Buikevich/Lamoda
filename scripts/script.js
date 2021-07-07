189bd806ea749217828b3454b89445150fccb46d
Aleksei-Buikevich <67165224+Aleksei-Buikevich@users.noreply.github.com>
2021-07-07 21:53:55 +0300

    день 2 (получение данных с сервера и вывод товара)

--- scripts/script.js
+++ scripts/script.js
@@ -1,20 +1,37 @@
 
 const headerCityButton = document.querySelector('.header__city-button');
 
-headerCityButton.textContent = localStorage.getItem('lomoda-location') || 'Ваш город?';
+let hash = location.hash.substring(1);
+
+
+
+const updateLocation = () => {
+  headerCityButton.textContent =  
+    localStorage.getItem('lomoda-location') ||
+    'Ваш город?';
+}
 
 headerCityButton.addEventListener('click', () => {
   const city = prompt('Укажите ваш город');
-  headerCityButton.textContent = city;
-  localStorage.setItem('lomoda-location', city)
+  if (city !== null){
+    localStorage.setItem('lomoda-location', city)
+  }
+  updateLocation()
 });
 
-
+updateLocation();
 // блокировка скролла
 
 const disableScroll = () => {
+
+if (document.disableScroll) return;
+
   const widthScroll = window.innerWidth - document.body.offsetWidth;
-  document.body.disableScrollY = window.scrollY
+
+  document.disableScroll = true;
+
+  document.body.disableScrollY = window.scrollY;
+
   document.body.style.cssText = `
     position: fixed;
     top: ${-window.scrollY}px;
@@ -27,6 +44,8 @@ const disableScroll = () => {
 };
 
 const enableScroll = () => {
+  document.disableScroll = false;
+
   document.body.style.cssText = '';
   window.scroll({
     top: document.body.disableScrollY,
@@ -50,6 +69,36 @@ const cartModalClose = () => {
   enableScroll();
 };
 
+
+// запрос базы данных
+
+const getData = async () => {
+  const data = await fetch('db.json');
+
+
+  if (data.ok) {
+    return data.json()
+  } else {
+    throw new Error(`данне небыли получены, ошибка ${data.status} ${data.statusText}`)
+  }
+};
+
+const getGoods = (callback, value) => {
+  getData('db.json')
+    .then(data => {
+      if (value) {
+        callback(data.filter(item => item.category === value))
+      } else {
+        callback(data);        
+      }
+
+    })
+    .catch(err =>{
+      console.error(err);
+    });
+};
+
+
 subheaderCart.addEventListener('click', cartModalOpen);
 
 cartOverlay.addEventListener('click', event => {
@@ -60,3 +109,65 @@ cartOverlay.addEventListener('click', event => {
   }
 });
 
+
+try {
+
+  const goodsList = document.querySelector('.goods__list');
+
+  if (!goodsList) {
+    throw `This is not a goods page!`
+  }
+
+  const creatCard = ({
+    id,
+    preview,
+    cost, 
+    brand, 
+    name, 
+    sizes 
+  }) => {
+
+    const li = document.createElement('li');
+
+    li.classList.add('goods__item');
+
+    li.innerHTML = `
+      <article class="good">
+        <a class="good__link-img" href="card-good.html#${id}">
+            <img class="good__img" src="goods-image/${preview}" alt="">
+        </a>
+        <div class="good__description">
+            <p class="good__price">${cost} &#8381;</p>
+            <h3 class="good__title">${brand} <span class="good__title__grey">/ ${name}</span></h3>
+            ${sizes ? 
+              `<p class="good__sizes">Размеры (RUS): <span class="good__sizes-list">${sizes.join(' ')}</span></p>` : 
+            ''}
+            <a class="good__link" href="card-good.html#${id}">Подробнее</a>
+        </div>
+      </article>
+    `;
+
+    return li;
+  };
+
+  const renderGoodsList = data => {
+    goodsList.textContent = '';
+
+    data.forEach(item =>{
+      const card = creatCard(item);
+      goodsList.append(card);
+    })
+
+  };
+
+  window.addEventListener('hashchange', () =>{
+    hash = location.hash.substring(1);
+    getGoods(renderGoodsList, hash);
+  })
+
+  getGoods(renderGoodsList, hash);
+
+
+} catch (err) {
+  console.warn(err)
+}
